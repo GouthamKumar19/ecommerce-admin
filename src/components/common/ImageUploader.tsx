0276923c4da722ab85137262d9d9553cd9745314
Jyothi0502 <suvarnajyothi2003@gmail.com>
2025-03-14 11:51:25 +0530

    image upload tooltip updated

--- src/components/common/ImageUploader.tsx
+++ src/components/common/ImageUploader.tsx
@@ -1,6 +1,6 @@
 // ImageUploader.tsx
 import React, { useState, useRef, DragEvent, ChangeEvent } from "react";
-import { Box, Typography, Button, Paper } from "@mui/material";
+import { Box, Typography, Button, Paper, Tooltip } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 interface ImageUploaderProps {
@@ -138,29 +138,34 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({
           >
             or
           </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            size="medium"
-            onClick={openFileInput}
-            disabled={isMaxImagesReached}
-            sx={{
-              px: 3,
-              py: 1,
-              textTransform: "none",
-              borderRadius: 1.5,
-              backgroundColor: "#4CAF50",
-              "&:hover": {
-                backgroundColor: "#3b8a3e",
-              },
-              "&.Mui-disabled": {
-                backgroundColor: "#cccccc",
-                color: "#666666",
-              },
-            }}
-          >
-            {isMaxImagesReached ? "Maximum Limit Reached" : "Browse Files"}
-          </Button>
+          <Tooltip
+              title=" You can upload up to 10 images"
+              arrow
+            >
+            <Button
+              variant="contained"
+              color="primary"
+              size="medium"
+              onClick={openFileInput}
+              disabled={isMaxImagesReached}
+              sx={{
+                px: 3,
+                py: 1,
+                textTransform: "none",
+                borderRadius: 1.5,
+                backgroundColor: "#4CAF50",
+                "&:hover": {
+                  backgroundColor: "#3b8a3e",
+                },
+                "&.Mui-disabled": {
+                  backgroundColor: "#cccccc",
+                  color: "#666666",
+                },
+              }}
+            >
+              {isMaxImagesReached ? "Maximum Limit Reached" : "Browse Files"}
+            </Button>
+            </Tooltip>
           <Typography
             variant="caption"
             color="textSecondary"
