9e70fd7be0630a5da2691e2d1ae2eefe45dfac3a
Anmol <anmolsshetty31@gmail.com>
2025-04-15 10:17:23 +0530

    validation fixed for slashed price

--- src/components/Product/ProductForm.tsx
+++ src/components/Product/ProductForm.tsx
@@ -283,6 +283,7 @@ const ProductForm: React.FC<ProductFormProps> = ({
               );
 
               if (validatePrice(priceValue)) {
+                // In the price onChange handler
                 if (
                   !numericSlashedPrice ||
                   numericPrice <= numericSlashedPrice
@@ -297,6 +298,21 @@ const ProductForm: React.FC<ProductFormProps> = ({
                   updateForm.setIsPriceValid(false);
                   updateForm.setPriceErrorMessage(
                     "Price must be less than the slashed price."
+                  );
+                }
+
+                // In the slashed price onChange handler
+                if (!numericPrice || numericSlashedPrice > numericPrice) {
+                  // Valid slashed price
+                  updateForm.setSlashedPrice(formatCurrency(slashedPrice));
+                  updateForm.setIsSlashedPriceValid(true);
+                  updateForm.setSlashedPriceErrorMessage("");
+                } else {
+                  // Slashed price is less than or equal to price
+                  updateForm.setSlashedPrice(formatCurrency(slashedPrice));
+                  updateForm.setIsSlashedPriceValid(false);
+                  updateForm.setSlashedPriceErrorMessage(
+                    "Slashed price must be greater than the price."
                   );
                 }
               } else {
