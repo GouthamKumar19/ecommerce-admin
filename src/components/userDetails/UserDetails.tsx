740efbf1837d6d4a4faa86a7fefe3c061484a36f
Anmol <anmolsshetty31@gmail.com>
2025-04-25 09:46:59 +0530

    added eye icon for password field

--- src/components/userDetails/UserDetails.tsx
+++ src/components/userDetails/UserDetails.tsx
@@ -3,11 +3,11 @@ import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import TextField from "@mui/material/TextField";
+import TextField from "@mui/material/TextField"; // Keep this import
 import AddressForm from "./AddressForm"; // Import the AddressForm component
 
 import {
-  InputAdornment,
+  InputAdornment, // Keep this import
   MenuItem,
   Select,
   FormControl,
@@ -17,6 +17,9 @@ import {
 import AddressPopup from "./AddressPopup";
 import { User } from "../../types/users.types";
 import { deleteUserAddresses } from "../../api/user";
+
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { IconButton } from "@mui/material"; // Keep only IconButton from this line
 
 interface AddressData {
   _id: string; // Add this line to include the _id field
@@ -340,6 +343,12 @@ const UserDetailsForm: React.FC<UserDetailsFormProps> = ({
     },
   };
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <div>
       {isLoading ? (
@@ -478,7 +487,7 @@ const UserDetailsForm: React.FC<UserDetailsFormProps> = ({
             </label>
             <TextField
               variant="outlined"
-              type="password"
+              type={showPassword ? "text" : "password"}
               name="password"
               placeholder="Password"
               value={formData.password}
@@ -489,6 +498,16 @@ const UserDetailsForm: React.FC<UserDetailsFormProps> = ({
               size="small"
               InputProps={{
                 style: { backgroundColor: "white" },
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleTogglePasswordVisibility}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
               }}
               sx={{
                 ...textFieldStyle,
